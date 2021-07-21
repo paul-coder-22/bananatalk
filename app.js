f343ec50a02797cc43931574353c14b2df628a16
your_username <kironpaul889@gmail.com>
2021-07-22 04:07:00 +0530

    CREATE Some DOm elements

---
+++ app.js
@@ -0,0 +1,9 @@
+/* let text = document.getElementById("btn").addEventListener("click", () => {
+    alert("Hello world");
+}) */
+// console.log(text)
+
+document.getElementById("btn").addEventListener("click", () => {
+    console.log(document.getElementById("text").value);
+
+})
