83d97609aaa84ce5a4e9404bf9af1f8ee4b3a9a0
your_username <kironpaul889@gmail.com>
2021-07-22 05:30:39 +0530

    Api intrigated

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+// const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+const serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
 document.getElementById("btn").addEventListener("click", () => {
     let textareaValue = document.getElementById("text").value;
-    document.getElementById("output").innerText = textareaValue;
+    fetch(getTranslationUrl(textareaValue))
+        .then(e => e.json())
+        .then(v => document.getElementById("output").innerText = v.contents.translated)
+        .catch(e => console.log("error in the serve !!!"))
+
 });
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
