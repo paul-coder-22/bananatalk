733530996b34d19cfd12b7188c54a9185aaa165c
your_username <MyEmail@Zoho.com>
2021-08-25 01:06:00 +0530

    Docs - Added Error Text

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-const serverUrl = "https://api.funtranslations.com/translate/minion.json"
+const serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-document.getElementById("btn").addEventListener("click", () => {
-    let textareaValue = document.getElementById("text").value;
-    fetch(getTranslationUrl(textareaValue))
-        .then(e => e.json())
-        .then(v => {
-            console.log(v.contents.translated)
-            console.log(document.getElementById("output"))
-            document.getElementById("output").innerText = v.contents.translated
-        })
-        .catch(e => console.log("error in the serve !!!"))
+const errorText = document.querySelector('.errorText');
+
+
+document.querySelector("#btn").addEventListener("click", () => {
+
+    let textareaValue = document.querySelector("#text").value;
+    if (textareaValue === '') {
+        errorText.style.display = "block"
+    } else {
+        errorText.style.display = "none"
+        fetch(getTranslationUrl(textareaValue))
+            .then(e => e.json())
+            .then(v => {
+                // console.log(v.contents.translated)
+                // console.log(document.getElementById("output"))
+                document.querySelector("#output").innerHTML = v.contents.translated
+            })
+            .catch(e => console.log("error in the server !!!"))
+    }
+
 });
 
 function getTranslationUrl(text) {
